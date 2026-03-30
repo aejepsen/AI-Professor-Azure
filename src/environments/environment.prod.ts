968f73bf389c881585f36b51f4fdfa954a82fce1
Allan Eric Jepsen <aejepsen@yahoo.com.br>
2026-03-29 21:22:56 -0300

    Delete src directory

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production:  true,
-  apiUrl:      '',   // Injetado pelo CI via ANGULAR_ENV_API_URL
-  clientId:    '',   // Injetado pelo CI via ANGULAR_ENV_CLIENT_ID
-  tenantId:    '',   // Injetado pelo CI via ANGULAR_ENV_TENANT_ID
-  apiScope:    '',   // Injetado pelo CI via ANGULAR_ENV_API_SCOPE
-  teamsTabUrl: '',
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-// src/environments/environment.ts
-export const environment = {
-  production: false,
-  apiUrl:      'http://localhost:8000',
-  clientId:    '',
-  tenantId:    '',
-  apiScope:    '',
-  teamsTabUrl: 'http://localhost:4200',
-};
